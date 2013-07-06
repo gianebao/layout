20b40c22d2aada7269f14396de7e41fd749efaa4
Gian Carlo Val Ebao <gianebao@gmail.com>
2013-07-06 20:41:24 +0800

    add url helper

---
+++ classes/Layout/URL.php
@@ -0,0 +1,27 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Layout URL helper.
+ *
+ * @package    Layout
+ * @category   Template
+ * @author     Gian Carlo Val Ebao <gianebao@gmail.com>
+ */
+class Layout_URL extends Kohana_URL {
+    
+/**
+ * Creates a URL link.
+ *
+ * @param string $route_name  Route name
+ * @param mixed  $params      Values to be passed
+ */
+    public static function href($route_name, $params = null)
+    {
+        if (!is_array($params))
+        {
+            $params = array('id' => $params);
+        }
+        
+        return Route::get($route_name)->uri($params);
+    }
+}
---
+++ classes/URL.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Layout URL helper.
+ *
+ * @package    Layout
+ * @category   Template
+ * @author     Gian Carlo Val Ebao <gianebao@gmail.com>
+ */
+class URL extends Layout_URL {}
